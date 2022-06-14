ee59cc8c71ddc4f317492537475d2ce0064d513b
mkyordanov <mkjordanov@gmail.com>
2022-06-14 11:04:13 +0300

    Initial commit of template Piranha template project

---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace TAU.Website.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# TAU EVS web site
