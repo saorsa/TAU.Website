be8eb5a81641b96102fe7c39cc202cc467b67c3d
mkyordanov <mkjordanov@gmail.com>
2022-08-02 16:20:24 +0300

    Added forms for newsletter and whitepaper

--- Controllers/NewsletterController.cs
+++ Controllers/NewsletterController.cs
@@ -1,26 +1,24 @@
-using TAU.Website.Models.Custom_Blocks;
-
 namespace TAU.Website.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
 using Piranha;
 using Piranha.AspNetCore.Services;
+using TAU.Website.Models.Custom_Blocks;
 
 public class NewsletterController : Controller
 {
     private readonly IApi _api;
     private readonly IModelLoader _loader;
 
-    public NewsletterController (IApi api, IModelLoader loader)
+    public NewsletterController(IApi api, IModelLoader loader)
     {
         _api = api;
         _loader = loader;
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> PostNewsletter(Newsletter newsletter)
     {
-        
         return Ok(newsletter);
     }
 }
